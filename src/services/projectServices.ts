97c9de9928388ab44920269cbb97604cc110f0eb
Guido <guidoxenofonte@gmail.com>
2026-04-13 17:07:52 -0300

    feat(api): add getProjectById function

--- src/services/projectServices.ts
+++ src/services/projectServices.ts
@@ -1,3 +1,4 @@
 import apiConnection from "./api";
 
-export const getProjects = () => apiConnection.get('/acoes')
+export const getProjects = () => apiConnection.get("/acoes");
+export const getProjectById = (id: string) => apiConnection.get(`/acoes/${id}`);
