813891cd17eb7e7a10e6628188c717b5ca0120cd
Guido <guidoxenofonte@gmail.com>
2026-04-29 17:13:23 -0300

    revert(api): revert to last commit

--- src/services/projectServices.ts
+++ src/services/projectServices.ts
@@ -24,7 +24,12 @@ export const searchProjectsByFilter = async (filters: ProjectFilters) => {
   try {
     const response = await apiConnection.get("/acoes", { params });
     const projects = response?.data?.data?.data;
-    
+    console.log(projects);
+    console.log(response);
+    console.log(response.data);
+    console.log(response.data?.data);
+    console.log(response.data?.data?.data);
+
     if (!projects) return [];
     return projects;
   } catch (error) {
