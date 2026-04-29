57efda35dba5c9f8129839781cee32cbc7fe1e1f
DanielNasc <danielnasc15987@gmail.com>
2026-04-29 00:54:30 -0300

    fix: repair vite config

--- vite.config.ts
+++ vite.config.ts
@@ -1,4 +1,4 @@
-import { defineConfig, loadEnv } from "vite";
+import { defineConfig } from "vite";
 import react from "@vitejs/plugin-react";
 import tailwindcss from "@tailwindcss/vite";
 import path from "path";
@@ -6,20 +6,10 @@ import path from "path";
 // https://vite.dev/config/
 export default defineConfig({
   plugins: [react(), tailwindcss()],
-  base: '/',
+  base: "/",
   resolve: {
     alias: {
       "@": path.resolve(__dirname, "./src"),
-    }
-  }
-
-  return {
-    plugins: [react(), tailwindcss()],
-    base: basePath,
-    resolve: {
-      alias: {
-        "@": path.resolve(__dirname, "./src"),
-      },
     },
-  };
+  },
 });
