d74566db4dc3db45d986b9744f9f7672507b17df
Guido <guidoxenofonte@gmail.com>
2026-04-28 22:27:29 -0300

    feat(.env): add .env for better global variables

--- src/services/api.ts
+++ src/services/api.ts
@@ -1,7 +1,7 @@
 import axios from "axios";
 
 const apiConnection = axios.create({
-  baseURL: "https://sigex.danielnasc.com.br/api",
+  baseURL: `${import.meta.env.VITE_API_URL}`,
   headers: { "Content-Type": "application/json" },
 });
 
