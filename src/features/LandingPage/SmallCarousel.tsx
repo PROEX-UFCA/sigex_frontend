63c2b37df7db8f57510133381a8fbecdc9def977
Guido <guidoxenofonte@gmail.com>
2026-05-05 14:13:49 -0300

    feat: add responsiveness and full API integration to landing page

--- src/features/LandingPage/SmallCarousel.tsx
+++ src/features/LandingPage/SmallCarousel.tsx
@@ -6,21 +6,77 @@ import {
   CarouselPrevious,
 } from "@/components/ui/carousel";
 
-import type { SmallCarouselProps } from "@/utils/smallCarousel";
 import SmallProject from "@/components/SmallProject";
 import { useScreenSize } from "@/hooks/useScreenSize";
 import { BREAKPOINTS } from "@/lib/breakpoints";
+import { useEffect, useState } from "react";
+import type { Projeto } from "@/types";
+import { searchProjectsByFilter } from "@/services/projectServices";
+import { Spinner } from "@/components/ui/spinner";
+import { TriangleAlert } from "lucide-react";
+import recognizeTags from "@/utils/tagRecognition";
+
+interface SmallCarouselSearchProps {
+  sectionTitle: string;
+  area_tematica?: string | null;
+  tipo_acao?: string | null;
+  data_inicio?: string | null;
+  data_fim?: string | null;
+}
 
 export default function SmallCarousel({
   sectionTitle,
-  itemList,
-}: SmallCarouselProps) {
+  area_tematica,
+  tipo_acao,
+  data_inicio,
+  data_fim,
+}: SmallCarouselSearchProps) {
   const { width } = useScreenSize();
+  const [results, setResults] = useState<Projeto[]>([]);
+  const [loading, setLoading] = useState<boolean>(true);
+
+  useEffect(() => {
+    const fetchData = async () => {
+      const data = await searchProjectsByFilter({
+        area_tematica: area_tematica ?? undefined,
+        tipo_acao: tipo_acao ?? undefined,
+        data_inicio: data_inicio ?? undefined,
+        data_fim: data_fim ?? undefined,
+      });
+      setResults(data);
+      setLoading(false);
+    };
+
+    fetchData();
+  }, []);
+
+  if (loading) {
+    return (
+      <div className="flex flex-col w-full items-center gap-2">
+        <Spinner className="w-12 h-12 text-gray-400" />
+        <p className="text-3xl text-gray-400">Buscando resultados...</p>
+      </div>
+    );
+  }
+
+  if (results.length == 0) {
+    return (
+      <div className="flex flex-col max-md:w-full md:w-47/50 items-center px-6 self-center gap-4">
+        <div className="flex self-start mb-2">
+          <h2 className="text-2xl font-bold text-[#553a25]">{sectionTitle}</h2>
+        </div>
+        <div className="flex flex-col w-2/3 self-center text-gray-400 my-4 py-4">
+          <TriangleAlert className="scale-400 mb-10 self-center" />
+          <p className="text-4xl">Não foi possível obter nenhum resultado</p>
+        </div>
+      </div>
+    );
+  }
 
   return (
-    <section className="w-full py-6">
+    <section className="w-full">
       <div
-        className={`flex flex-col max-md:w-full md:max-w-47/50 mx-auto px-6 items-center`}
+        className={`flex flex-col max-md:w-full md:w-47/50 mx-auto px-6 items-center`}
       >
         <div className="flex self-start mb-2">
           <h2 className="text-2xl font-bold text-[#553a25]">{sectionTitle}</h2>
@@ -46,7 +102,7 @@ export default function SmallCarousel({
             className="w-full"
           >
             <CarouselContent className="-ml-2 md:-ml-4">
-              {itemList.map((item, index) => (
+              {/* {itemList.map((item, index) => (
                 <CarouselItem
                   key={index}
                   className="pl-2 md:pl-4 max-md:basis-[45%] md:basis-[32%] lg:basis-[23%] xl:basis-[19%] 2xl:basis-[15%]"
@@ -56,6 +112,19 @@ export default function SmallCarousel({
                     id={item.id}
                     tags={item.tags}
                     title={item.title}
+                  />
+                </CarouselItem>
+              ))} */}
+              {results.map((item, index) => (
+                <CarouselItem
+                  key={index}
+                  className="pl-2 md:pl-4 max-md:basis-[45%] md:basis-[32%] lg:basis-[23%] xl:basis-[19%] 2xl:basis-[15%]"
+                >
+                  <SmallProject
+                    key={item.id}
+                    id={item.id}
+                    tags={recognizeTags([item.area_tematica])}
+                    title={item.titulo}
                   />
                 </CarouselItem>
               ))}
