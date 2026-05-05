d676c594c95e3e754301530d193bd89187480138
Guido <guidoxenofonte@gmail.com>
2026-05-05 13:15:36 -0300

    feat: fixes on footer and main app

--- src/components/Footer.tsx
+++ src/components/Footer.tsx
@@ -1,39 +1,38 @@
 import logoProex from "@/assets/proex-branco_total_teste.png";
+
 import { useScreenSize } from "@/hooks/useScreenSize";
-import { getActiveBreakpoint } from "@/utils/breakpoints";
+import { BREAKPOINTS } from "@/lib/breakpoints";
 
 export default function Footer() {
   const { width } = useScreenSize();
-  const windowSize = getActiveBreakpoint(width);
   
-
   return (
     <footer className="w-full text-white">
       <div className="flex flex-col h-fit bg-[#532b1d] gap-4">
         <div className="flex flex-row w-full lg:px-16 md:px-8 sm:px-4 px-2">
-          <div className="justify-start lg:w-1/3 md:w-1/3 sm:w-1/2 w-1/2">
+          <div className="justify-start lg:w-1/3 w-1/2">
             <img
               src={`${logoProex}`}
               className="left-0 h-48 object-contain"
             ></img>
           </div>
-          <div className="justify-start lg:w-1/3 md:w-1/3 sm:w-1/2 w-1/2 text-left text-lg py-6">
+          <div className="justify-start w-1/2 lg:w-1/3 text-left text-lg py-6">
             Endereço: Av. Tenente Raimundo Rocha N° 1639 &ndash; Bairro Cidade
             Universitária Juazeiro do Norte &ndash; Ceará &ndash; CEP: 63048-080
             &ndash; Bloco "K", Salas K201 e K202 (2° Andar) &ndash; Telefone:
             (88) 3221-9286
           </div>
-          {(windowSize == "xs" || windowSize == "sm") ? (
+          {(width > BREAKPOINTS.lg) ? (
             <div className="justify-start w-1/3"></div>
           ) : (
-            <></>
+            null
           )}
         </div>
         <div className="flex flex-row w-full lg:px-16 md:px-8 sm:px-4 px-2">
           <div className="text-left lg:text-lg md:text-lg sm:text-md text-sm w-2/3 font-bold">
             &copy; 2026 Pró-Reitoria de Extensão da UFCA
           </div>
-          <div className="text-right lg:text-lg md:text-lg sm:text-md text-sm w-1/3 font-bold">
+          <div className="text-right lg:text-lg md:text-lg sm:text-md text-sm w-2/3 font-bold">
             Design by Guido Xenofonte
           </div>
         </div>
