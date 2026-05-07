e4b2ec24c957e2ac5eeb662d16d36dae7ddace6c
Guido <guidoxenofonte@gmail.com>
2026-05-07 13:44:24 -0300

    fix: fixed search area missing form component for input when on specific screen size

--- src/components/topbar/TopBar.tsx
+++ src/components/topbar/TopBar.tsx
@@ -93,10 +93,14 @@ function SearchArea({
         ></ToggleableSearchBar>
       )}
       {width >= BREAKPOINTS.md && width < BREAKPOINTS.lg && (
-        <Input
-          placeholder="Pesquise projetos ou áreas de atuação"
-          className="border-gray-400 bg-gray-100 font-bold w-3/5 h-10"
-        ></Input>
+        <form className="w-3/5" onSubmit={handleSearch}>
+          <Input
+            value={term}
+            onChange={(e) => setTerm(e.target.value)}
+            placeholder="Pesquise projetos ou áreas de atuação"
+            className="border-gray-400 bg-gray-100 font-bold w-full h-10"
+          ></Input>
+        </form>
       )}
       {width >= BREAKPOINTS.lg && (
         <form className="w-2/5" onSubmit={handleSearch}>
