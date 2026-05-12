999e1084073ecbe143dc849f102d263fe84d6a9e
Guido <guidoxenofonte@gmail.com>
2026-05-12 10:58:17 -0300

    feat: add tag recognition related tests

---
+++ src/utils/tagRecognition.test.ts
@@ -0,0 +1,28 @@
+import { describe, expect, it } from "vitest";
+import recognizeTags from "./tagRecognition";
+
+describe("Tag Recognition Tests", () => {
+  it('Recognizes "Tecnologia da Informação"', () => {
+    expect(recognizeTags(["Tecnologia da Informação"])).toEqual([
+      { tagType: "Tecnologia" },
+    ]);
+  });
+
+  it('Generates multiple tags for "Educação e Cultura"', () => {
+    const result = recognizeTags(["Educação e Cultura"]);
+    expect(result).toContainEqual({ tagType: "Cultura" });
+    expect(result).toContainEqual({ tagType: "Educação" });
+  });
+
+  it("Should be case-insensitive", () => {
+    expect(recognizeTags(["TECNOLOGIA"])).toEqual([{ tagType: "Tecnologia" }]);
+  });
+
+  it("Should return an empty array when given a string without category", () => {
+    expect(recognizeTags(["Equidade"])).toEqual([]);
+  });
+
+  it("Should not raise an error when given an empty array", () => {
+    expect(recognizeTags([])).toEqual([]);
+  });
+});
