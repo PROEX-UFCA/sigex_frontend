a40f8930336126ba4959f46d7a5504f946b28914
Guido <119985142+guidoxenofonte2005@users.noreply.github.com>
2026-06-11 12:17:47 -0300

    fix: add null safety to ODS grid

--- src/components/ODSGrid.tsx
+++ src/components/ODSGrid.tsx
@@ -1,7 +1,9 @@
 const ODS_QUANTITY: number = 17;
 
 export default function ODSGrid({ odsListStr }: { odsListStr: string }) {
-  const odsList: string[] = odsListStr.split("; ").sort();
+  const odsList: string[] | null = odsListStr.split("; ").sort();
+
+  if (!odsList) return <></>;
 
   const hasSpecificODS: boolean[] = [];
   for (let i: number = 1; i <= ODS_QUANTITY; i++) {
@@ -12,9 +14,15 @@ export default function ODSGrid({ odsListStr }: { odsListStr: string }) {
   return (
     <div className="grid self-center items-center max-lg:w-4/5 lg:w-3/5 grid-cols-6 gap-x-0 mx-0">
       {hasSpecificODS.map((value, index) => (
-        <img src={`https://sig.ufca.edu.br/sigaa/img/ODS/${value ? `${index + 1}` : `${index + 1}_light`}.png`} className="w-full"></img>
+        <img
+          src={`https://sig.ufca.edu.br/sigaa/img/ODS/${value ? `${index + 1}` : `${index + 1}_light`}.png`}
+          className="w-full"
+        ></img>
       ))}
-      <img src="https://sig.ufca.edu.br/sigaa/img/ODS/ods_.png" className="w-full"></img>
+      <img
+        src="https://sig.ufca.edu.br/sigaa/img/ODS/ods_.png"
+        className="w-full"
+      ></img>
     </div>
   );
 }
